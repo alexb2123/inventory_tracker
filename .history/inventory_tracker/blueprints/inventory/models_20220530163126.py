488952db51f109fc938f6036ada402b23870be4c
alex bordei <alexb2123@gmail.com>
2022-05-30 17:08:58 -0400

    i think i managed to get db updates to work

---
+++ .history/inventory_tracker/blueprints/inventory/models_20220530163126.py
@@ -0,0 +1,10 @@
+from ... import db
+
+class Inventory(db.Model):
+    __tablename__ = "Inventory"
+    __bind_key__ = "purchase_orders"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    item_name = db.Column(db.String(100))
+    item
+    
